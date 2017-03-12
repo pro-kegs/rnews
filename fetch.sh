0a5c979f84bfc9f2199ed84a2df7fce52950d12c
Kelvin Sherlock <ksherlock@gmail.com>
2017-03-11 22:35:10 -0500

    clean up old stuff from fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-SERVER=news.eternal-september.org 
-NNTP_USER=
-NNTP_PASS=
 
+# suck flags:
 # -Q - use NNTP_USER / NNTP_PASS
 # -c update sucknewsrc
 # -dd xxx data directory
@@ -10,39 +8,30 @@ NNTP_PASS=
 # -br xxx rnews batch
 # -r xxx size limit for rnews batch files
 # -y xxx filter program
-suck $SERVER -Q -c -dd es/ -dt tmp -br out/es.rnews -r 10000 -y ./filter.pl
 
-SERVER=news.gwene.org
-NNTP_USER=
-NNTP_PASS=
-suck $SERVER -Q -c -dd gwene/ -dt tmp -br out/gwene.rnews -r 10000 -y ./filter.pl
+#
+# directory layout:
+# servers/[name]/config.sh
+# servers/[name]/sucknewsrc
+# 
+# where name is something short (so [name].rnews will be a valid ProDOS name)
+# config.sh should look like:
+# NNTP_SERVER=....
+# NNTP_USER=...
+# NNTP_PASS=...
+#
 
-###
-
-# servers/es
-# servers/gwene
-# cat servers/es
-# SERVER=
-# NNTP_USER=
-# NNTP_PASS=
-
-mkdir tmp
-mkdir out
-mkdir msgs
+mkdir -p tmp
+mkdir -p out
+mkdir -p msgs
 for x in servers/* ; do
-	SERVER=""
+	NNTP_SERVER=""
 	NNTP_USER=""
 	NNTP_PASS=""
-	name=${x##servers/} #
-	source "$x/config"
-	suck $SERVER -Q -c -dd "$x" -dt tmp -dm msgs -br "out/${name}.rnews" -r 10000 -y ./filter.pl
-done
-
-
-for x in servers/* ; do
-
-	# ${parameter##word} -- removes largest prefix.
-	for k in $x/env/* ; do ${k##*/}=`cat "$k"`
-
-
+	name=${x##servers/}
+	if [ -e "$x/config.sh" ] ; then . "$x/config.sh" ; fi
+	if [ -n "$NNTP_SERVER" ] ; then
+		echo "Reading ${name}"
+		suck $NNTP_SERVER -Q -c -dd "$x" -dt tmp -dm msgs -br "out/${name}.rnews" -r 10000 -y ./filter.pl
+	fi
 done
